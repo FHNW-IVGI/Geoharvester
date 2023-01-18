e3a559db4c41e27322b61a02c4e975b99a1cbb09
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2023-01-18 14:55:34 +0100

    Fix wrong url for services

--- server/app/constants.py
+++ server/app/constants.py
@@ -7,13 +7,13 @@ url_github_repo_suffix = "?raw=true"
 
 url_geodata_CH_csv = "{}geodata_CH.csv{}".format(url_github_repo, url_github_repo_suffix)
 url_geodata_simple_CH_csv = "{}geodata_simple_CH.csv{}".format(url_github_repo,url_github_repo_suffix)
-url_geoservices_CH_csv = "{}geodata_CH.csv{}".format(url_github_repo,url_github_repo_suffix)
-url_geoservices_stats_CH_csv = "{}geodata_CH.csv{}".format(url_github_repo,url_github_repo_suffix)
+url_geoservices_CH_csv = "{}geoservices_CH.csv{}".format(url_github_repo,url_github_repo_suffix)
+url_geoservices_stats_CH_csv = "{}geoservices_CH.csv{}".format(url_github_repo,url_github_repo_suffix)
 
 
 #####  
 # Variables:
 
 # Used to create a subset of the full csv data:
-fields_to_include = ["OWNER","TITLE", "ABSTRACT"]
-fields_to_output = ["OWNER","TITLE"] # Needs to be subset of fields_to_include
+fields_to_include = ["OWNER","TITLE", "ABSTRACT", "SERVICETYPE", "SERVICELINK"]
+fields_to_output = ["OWNER","TITLE", "SERVICETYPE", "SERVICELINK"] # Needs to be subset of fields_to_include
