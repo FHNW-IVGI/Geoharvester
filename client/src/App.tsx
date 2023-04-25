b0dc665bcf96704ec3624c36e7e74a9800bafe41
Elia Ferrari <elia.ferrari@fhnw.ch>
2023-04-25 08:20:50 +0200

    Adjustment with main branch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { SearchBar } from "./components/search/SearchBar";
 import { ResultArea, StatisticsBox } from "./components/results/ResultArea";
+<<<<<<< HEAD
+=======
+import { getServerStatus } from "./requests";
+>>>>>>> 35991c4 (Adjustment with main branch)
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { MenuBar } from "./components/menu/MenuBar";
 import { Geoservice } from "./types";
@@ -19,7 +23,11 @@ const theme = createTheme({
       // main: "#ffbe92",
       main: "#e69138",
       contrastText: "#000000",
+<<<<<<< HEAD
     },
+=======
+    }
+>>>>>>> 35991c4 (Adjustment with main branch)
   },
 });
 
@@ -33,14 +41,35 @@ export type SearchResult = {
 function App() {
   const [searchResult, setSearchResult] = useState({} as SearchResult);
 
+<<<<<<< HEAD
+=======
+  const checkServerStatus = () =>
+    getServerStatus()
+      .then((res) => {
+        setStatusString(res ? res : "error");
+      })
+      .catch((e) => console.error(e));
+
+  useEffect(() => {
+    checkServerStatus();
+  }, []);
+
+>>>>>>> 35991c4 (Adjustment with main branch)
   const { docs, total, fields } = searchResult;
 
   return (
     <ThemeProvider theme={theme}>
+<<<<<<< HEAD
       {/* <ThemeProvider theme={theme}> */}
       <header className="App-header">
         {/* NDGI Project Geoharvester */}
         <MenuBar />
+=======
+    {/* <ThemeProvider theme={theme}> */}
+      <header className="App-header">
+        {/* NDGI Project Geoharvester */}
+        <MenuBar/>
+>>>>>>> 35991c4 (Adjustment with main branch)
       </header>
       {/* <div>
       <FilterAltIcon variant="contained">Hello World</FilterAltIcon>
@@ -50,6 +79,12 @@ function App() {
         <StatisticsBox total={total || 0}></StatisticsBox>
         <ResultArea docs={docs || [[]]} fields={fields}></ResultArea>
       </main>
+<<<<<<< HEAD
+=======
+      <footer className="App-footer">
+        <Footer status={statusString} checkServerStatus={checkServerStatus} />
+      </footer>
+>>>>>>> 35991c4 (Adjustment with main branch)
     </ThemeProvider>
   );
 }
--- client/src/requests.ts
+++ client/src/requests.ts
@@ -3,7 +3,13 @@ import axios from "axios";
 
 
 const routes = {
+<<<<<<< HEAD
     getData: `/api/getData`,
+=======
+    getServerStatus: `/api`,
+    getDataFromPandas: `/api/getDataFromPandas`,
+    getDataFromRedis: `/api/getDataFromRedis`,
+>>>>>>> 35991c4 (Adjustment with main branch)
 }
 
 enum LANG {
@@ -11,6 +17,7 @@ enum LANG {
     ENG = "english",
     FR = "french",
     IT = "italian"
+<<<<<<< HEAD
 }
 
 // Redit returns 10 results by default, use this fallback instead when no value is given
@@ -19,6 +26,20 @@ const LIMIT = 100
 
 export const getData = async (query: string, lang: string = LANG.GER, resultLimit: number = LIMIT) => {
     const result = await axios(routes.getData, { params: { query, lang, resultLimit } });
+=======
+}
+
+// Redit returns 10 results by default, use this fallback instead when no value is given
+const LIMIT = 100
+
+export const getServerStatus = async () => {
+    const result = await axios(routes.getServerStatus, {});
+    const { message } = result.data;
+    return message
+}
+export const getData = async (query: string, lang: string = LANG.GER, resultLimit: number = LIMIT) => {
+    const result = await axios(routes.getDataFromRedis, { params: { query, lang, resultLimit } });
+>>>>>>> 35991c4 (Adjustment with main branch)
     const { data } = result;
     return data
 }
