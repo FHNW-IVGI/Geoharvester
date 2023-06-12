535789d77d15a481836f027b34f5f162548a1212
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2023-06-12 12:00:26 +0200

    Reset Menubar

--- client/src/components/menubar/MenuBar.tsx
+++ client/src/components/menubar/MenuBar.tsx
@@ -103,72 +103,6 @@ export const MenuBar = ({
             onChange={(e) => setSearchString(e.target.value)}
             onKeyDown={(e) =>
               e.key === "Enter" &&
-              triggerSearch(searchStringState, undefined, undefined)
-            }
-            startAdornment={
-              <SearchIcon style={{ marginLeft: -8, marginRight: 6 }} />
-            }
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="clear search"
-                  onClick={() => setSearchString("")}
-                >
-                  <CancelIcon style={{ marginRight: -14, marginLeft: -8 }} />
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-          <SearchButton
-            id="search-button"
-            size="small"
-            onClick={() =>
-              triggerSearch(searchStringState, undefined, undefined)
-            }
-            sx={{
-              fontSize: 14,
-              backgroundColor: "white",
-              color: theme.palette.primary.main,
-            }}
-            type="submit"
-            variant="outlined"
-            aria-label="search"
-          >
-            {"Suchen"}
-          </SearchButton>
-        </FormControl>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "end",
-          width: 500,
-        }}
-      >
-
-        <div style={{ width: "30%", display: "flex", alignItems: "center" }}>
-          <IconButton
-            size="large"
-            edge="end"
-            aria-label="menu"
-            sx={{ mr: 1, color: theme.palette.secondary.main }}
-            onClick={handleClick}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            style={{ marginLeft: -16 }}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-            onChange={(e) => setSearchString(e.target.value)}
-            onKeyDown={(e) =>
-              e.key === "Enter" &&
               triggerSearch(searchStringState, undefined, undefined, page)
             }
             startAdornment={
@@ -191,14 +125,6 @@ export const MenuBar = ({
             onClick={() => {
               resetPageToZero();
               triggerSearch(searchStringState, undefined, undefined, page);
-        <div style={{ display: "flex", flex: "1 1 auto" }}>
-          <FormControl
-            sx={{
-              m: 1,
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
             }}
             sx={{
               fontSize: 14,
@@ -212,54 +138,6 @@ export const MenuBar = ({
             {"Suchen"}
           </SearchButton>
         </FormControl>
-      </div>
-      <Filter
-        handleChangeService={handleChangeService}
-        handleChangeProvider={handleChangeProvider}
-        provider={providerState}
-        servicetype={servicetypeState}
-      />
-            }}
-          >
-            {PROVIDERLIST.map((provider) => {
-              return (
-                <MenuItem key={provider} value={provider}>
-                  {provider}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <FormControl
-          variant="outlined"
-          sx={{
-            minWidth: 140,
-          }}
-        >
-          <Select
-            autoComplete="off"
-            defaultValue={""}
-            labelId="select-service-label"
-            id="select-service"
-            value={servicetype}
-            onChange={handleChangeService}
-            style={{
-              backgroundColor: "white",
-              textAlign: "center",
-              height: 32,
-              color: "#007CC3",
-            }}
-          >
-            {SERVICELIST.map((servicetype) => {
-              return (
-                <MenuItem key={servicetype} value={servicetype}>
-                  {servicetype}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-        <div style={{ width: 12 }} />
       </div>
       <Filter
         handleChangeService={handleChangeService}
