de92222d647bdf604ef7394c054fca6f9151a487
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2023-06-15 10:39:37 +0200

    Basic cron job

---
+++ server/cronjob/cronjob.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+if __name__ == "__main__":
+    print(f"Running script at {datetime.now()}")
+    print("This message is sent every 1 minutes", "<h1>Hello World from a Docker container</h1>")
