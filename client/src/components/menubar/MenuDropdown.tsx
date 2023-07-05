fa471a29bcef8b8a6a06c51827b220181965e89b
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2023-07-05 13:38:55 +0200

    Hardcoded url

--- client/src/components/menubar/MenuDropdown.tsx
+++ client/src/components/menubar/MenuDropdown.tsx
@@ -60,7 +60,7 @@ export const MenuDropdown = () => {
         <MenuItem
           onClick={() => {
             setAnchorEl(null);
-            window.location.replace(`${window.location.origin}/api/docs/`); // This should work for the prod version
+            window.location.replace("http://3.76.227.122/api/docs/"); // This should work for the prod version for now
           }}
         >
           <TerminalIcon style={{ marginRight: 14 }} />
