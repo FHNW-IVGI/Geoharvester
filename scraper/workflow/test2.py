ef35db65e2ec21c9c35c628872626f04a0f3a21d
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2024-07-15 15:27:37 +0200

    Debugging

--- scraper/workflow/test2.py
+++ scraper/workflow/test2.py
@@ -10,23 +10,19 @@ if __name__ == "__main__":
     language = os.environ['LANG_FROM_PIPELINE']
 
 
-    print("1", os.path.join(config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data',f'{language}_preprd_data.pkl'))
 
     try:    
         dir_list = os.listdir(os.path.join(config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data'))
         print("2", dir_list)
     finally: 
         print()
+
     try:
-        dir_list = os.listdir(os.path.join(config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data',f'{language}_preprd_data'))
-        print("3", dir_list)
-    finally: 
-        print()
-    try:
-        dir_list = os.listdir(os.path.join(os.getcwd(), config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data',f'{language}_preprd_data'))
+        dir_list = os.listdir(os.path.join(os.getcwd(), config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data'))
         print("4", dir_list)
     finally: 
         print()
 
+    print("1", os.path.join(config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data.pkl'))
     preprd_data = pd.read_csv(os.path.join(config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data',f'{language}_preprd_data.csv'))
     print("4", preprd_data)
