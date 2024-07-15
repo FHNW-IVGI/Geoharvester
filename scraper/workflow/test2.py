692ceba5283393c822f20474b00af49858fae9c0
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2024-07-15 15:34:02 +0200

    Debugging

--- scraper/workflow/test2.py
+++ scraper/workflow/test2.py
@@ -17,11 +17,7 @@ if __name__ == "__main__":
     finally: 
         print()
 
-    try:
-        dir_list = os.listdir(os.path.join(os.getcwd(), config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data'))
-        print("4", dir_list)
-    finally: 
-        print()
+
 
     print("1", os.path.join(config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data.csv'))
     preprd_data = pd.read_csv(os.path.join(config.WORKFLOW_ARTIFACT_FOLDER,f'{language}_preprd_data.csv'))
