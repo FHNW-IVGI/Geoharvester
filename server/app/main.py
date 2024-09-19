272eb160fa42eedc1d63138a026fb5cd4bedec65
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2024-09-19 10:04:08 +0200

    Reset data repo to actual data

--- server/app/main.py
+++ server/app/main.py
@@ -75,8 +75,7 @@ async def startup_event():
     r.ft().config_set("MAXSEARCHRESULTS", "-1" )
 
     global dataframe
-    url_github_repo = "https://raw.githubusercontent.com/FHNW-IVGI/Geoharvester/main/scraper/temp_data/" # Temporary fix
-    #url_github_repo = "https://raw.githubusercontent.com/FHNW-IVGI/Geoharvester/main/scraper/data/" # Restore once pipeline works
+    url_github_repo = "https://raw.githubusercontent.com/FHNW-IVGI/Geoharvester/main/scraper/data/" # Restore once pipeline works
     url_geoservices_CH_pkl = os.path.join(url_github_repo, "merged_data.pkl")
     dataframe = import_pkl_into_dataframe(url_geoservices_CH_pkl)
     # url_geoservices_CH_csv = "app/tmp/geoservices_CH.csv"
